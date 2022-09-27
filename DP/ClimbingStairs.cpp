24e29a88e8a432343f3bcc118360f55e8d5c2ff3
PrernaCodifier <33116849+Prernapundir@users.noreply.github.com>
2022-09-27 20:32:19 +0530

    Create ClimbingStairs.cpp

---
+++ DP/ClimbingStairs.cpp
@@ -0,0 +1,23 @@
+/*fibonacci approach
+
+0 1 1 2 3 5 8....
+
+*/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2) return n;
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+        
+    }
+};
